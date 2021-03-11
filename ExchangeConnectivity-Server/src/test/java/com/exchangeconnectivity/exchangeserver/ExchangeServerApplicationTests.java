4cf5e3016e1d1b1fad3d6f8274d9f99eb044bf06
= <=>
2021-03-11 19:48:23 +0000

    added spring boot dependencies and initialized basic spring app

---
+++ ExchangeConnectivity-Server/src/test/java/com/exchangeconnectivity/exchangeserver/ExchangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangeconnectivity.exchangeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
