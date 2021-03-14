a9e2802fe81e4351d0b124c01306ea0bc7d1d4db
= <=>
2021-03-14 18:43:05 +0000

    modified port number and added request to trade engine api

--- order-validation-server/src/main/java/com/ordervalidation/ordervalidationserver/ServiceEndpoint.java
+++ order-validation-server/src/main/java/com/ordervalidation/ordervalidationserver/ServiceEndpoint.java
@@ -3,23 +3,49 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import turntabl.trading.ordervalidservice.ValidateOrder;
 import turntabl.trading.ordervalidservice.ValidationResponse;
 
+import java.io.IOException;
+
 @Endpoint
 public class ServiceEndpoint {
+    Retrofit retrofit = new Retrofit.Builder().baseUrl("").addConverterFactory(GsonConverterFactory.create()).build();
+
     private static final String NAMESPACE_URI = "http://turntabl/trading/ordervalidservice";
 
-    public ServiceEndpoint(){
-
-    }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "validateOrder")
     @ResponsePayload
     public ValidationResponse validateOrder(@RequestPayload ValidateOrder request){
-        ValidationResponse response = new ValidationResponse();
-        response.setOrderID("2fb-00-56-ui-URI2");
-        response.setOrderstatus("Order validated successfully");
-        return response;
+        if(validate(request)){
+            String orderID = sendOrderRequest(request);  //make request to Trade engine sending body {id, name, quantity, price, side}
+            ValidationResponse response = new ValidationResponse();
+            response.setOrderID(orderID);
+            response.setOrderstatus("Order validated");
+            return response;
+        }
+            ValidationResponse response = new ValidationResponse();
+            response.setOrderID("");
+            response.setOrderstatus("Order failed validation");
+            return response;
     }
+
+    public String sendOrderRequest(ValidateOrder order){
+        PostOrderService.orderService service = retrofit.create(PostOrderService.orderService.class);
+        Call<String> req = service.sendOrderRequest(order);
+        try {
+            return req.execute().body();
+        }catch (IOException e) {
+            e.printStackTrace();
+            return "order failed";
+        }
+    }
+
+    public Boolean validate(ValidateOrder request){return true; }  //this function should handle validation of order before even placing it.
 }
+
+
