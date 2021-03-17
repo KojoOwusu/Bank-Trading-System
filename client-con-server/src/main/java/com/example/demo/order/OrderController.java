b8d7ba33759a45eec07962e20ecd33b0727a34c0
= <=>
2021-03-17 17:39:28 +0000

    added SOAP request to client service in POST controller

--- client-con-server/src/main/java/com/example/demo/order/OrderController.java
+++ client-con-server/src/main/java/com/example/demo/order/OrderController.java
@@ -4,48 +4,43 @@ package com.example.demo.order;
 import java.util.Arrays;
 import java.util.List;
 
+import com.example.consumingorderservice.wsdl.ValidationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class OrderController {
-	@Autowired
-	private OrderService orderservice;
-	
-	
-	@RequestMapping("/")
-	public String helloworld(){
 
-		
-		return "HelloWorld";
-		
-	}
-	
-	@RequestMapping("/orders")
+	@Autowired
+	OrderSOAPClient soapClient;
+
+
+/*
+	@RequestMapping("/api/orders")
 	public List <Order> getAllOrders() {
 
-		
+
 		return orderservice.getAllOrder();
-		
+
 	}
-	
-	@RequestMapping("/orders/{id}")
-	public Order getOrderbyid(@PathVariable String id) {
-		return orderservice.getorderbyid(id);
-	}
+*/
+//	@RequestMapping("/api/orders/{id}")
+//	public Order getOrderbyid(@PathVariable String id) {
+//		return orderservice.getorderbyid(id);
+	//}
 
 	
-	@RequestMapping(method=RequestMethod.POST, value="/orders")
-	public void addTopic(@RequestBody Order order) {
-		
-		System.out.print("I received the request" + order);
-		
-	//orderservice.addOrder(order);
+	@RequestMapping(method=RequestMethod.POST, value="/api/order", consumes = "application/json", produces = "application/json")
+	@ResponseBody
+	public ValidationResponse sendOrder(@RequestBody Order order) {
+		String productName = order.getProduct();
+		int quantity = order.getQuantity();
+		double price = order.getPrice();
+		String side = order.getSide();
+
+
+		var response = soapClient.setOrder(productName, quantity, price, side);
+		return response;
 	}
 }
