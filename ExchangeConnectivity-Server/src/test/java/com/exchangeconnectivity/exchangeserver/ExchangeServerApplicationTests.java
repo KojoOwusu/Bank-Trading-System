f77ae667c5fe43ad0fbb9144223d51a52f076142
= <=>
2021-03-18 17:19:19 +0000

    added test to exchange connectivity

--- ExchangeConnectivity-Server/src/test/java/com/exchangeconnectivity/exchangeserver/ExchangeServerApplicationTests.java
+++ ExchangeConnectivity-Server/src/test/java/com/exchangeconnectivity/exchangeserver/ExchangeServerApplicationTests.java
@@ -1,13 +1,66 @@
 package com.exchangeconnectivity.exchangeserver;
 
+import com.exchangeconnectivity.exchangeserver.resourceclasses.OrderResponse;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.context.WebApplicationContext;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={ExchangeServerApplication.class})
+@AutoConfigureMockMvc
 class ExchangeServerApplicationTests {
+	@Autowired
+	OrderController orderController;
+
+	@Autowired
+	public MockMvc mockMvc;
+
+
+	@Mock
+	APIInterfaces.GetOrder orderInterface;
+	Call<OrderResponse> response;
+	Response res;
+	OrderResponse data;
+
+	@Before
+	public void init() throws IOException {
+		when(orderInterface.getOrderStatus(Mockito.anyString())).thenReturn(response);
+		when(response.execute()).thenReturn(res);
+		when(res.body()).thenReturn(data);
+	}
 
 	@Test
 	void contextLoads() {
+		assertThat(orderController).isNotNull();
+	}
+
+	@Test
+	public void testOrder() throws Exception {
+		this.mockMvc.perform( MockMvcRequestBuilders
+				.get("/api/getorder?ID=ac5f1f242-d894-4c13-a1c")
+				.accept(MediaType.APPLICATION_JSON))
+				.andDo(print()).andExpect(status().isOk());
 	}
 
 }
