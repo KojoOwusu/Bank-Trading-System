42cb2b723a808a12b7f9b78fe4cce5d75820b09d
= <=>
2021-03-18 15:52:24 +0000

    added test

--- order-validation-server/src/test/java/com/ordervalidation/ordervalidationserver/OrderValidationServerApplicationTests.java
+++ order-validation-server/src/test/java/com/ordervalidation/ordervalidationserver/OrderValidationServerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ordervalidation.ordervalidationserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class OrderValidationServerApplicationTests {
+	@Autowired
+	private ServiceEndpoint serviceEndpoint;
+	private ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet;
+	private DefaultWsdl11Definition defaultWsdl11Definition;
 
 	@Test
 	void contextLoads() {
+			assertThat(serviceEndpoint).isNotNull();
+			//assertThat(messageDispatcherServlet).isNot()
+		//	assertThat(defaultWsdl11Definition).isNotNull();
+
+
 	}
 
 }
