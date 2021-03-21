e868fd51e4113bbe685bd3a7ab94ec82f21a6047
= <=>
2021-03-21 19:43:50 +0000

    made jedis client connect to remote and added post mapping for webhooks

--- TradeServices/src/main/java/tradeengine/TradeServices/jedisconfig/JedisConfig.java
+++ TradeServices/src/main/java/tradeengine/TradeServices/jedisconfig/JedisConfig.java
@@ -9,11 +9,9 @@ public class JedisConfig {
 
     @Bean
     public static Jedis createJedisClient(){
-        Jedis jedisclient = new Jedis();
-       // jedisclient.auth("fcTHon925fcjUDen1ujM4x5Ra1PsJYIk");
+        Jedis jedisclient = new Jedis( "redis-14349.c81.us-east-1-2.ec2.cloud.redislabs.com", 14349);
+       jedisclient.auth("fcTHon925fcjUDen1ujM4x5Ra1PsJYIk");
         return jedisclient;
     }
 }
 
-
-// "redis-14349.c81.us-east-1-2.ec2.cloud.redislabs.com", 14349
