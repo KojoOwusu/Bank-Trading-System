2b96f89ca3bff95b145298d9f0888f4501cc4f61
= <=>
2021-03-21 16:01:01 +0000

    fixing errors

--- ExchangeConnectivity-Server/src/test/java/com/exchangeconnectivity/exchangeserver/ExchangeServerApplicationTests.java
+++ ExchangeConnectivity-Server/src/test/java/com/exchangeconnectivity/exchangeserver/ExchangeServerApplicationTests.java
@@ -4,23 +4,14 @@ import com.exchangeconnectivity.exchangeserver.resourceclasses.OrderStatus;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 import java.io.IOException;
 
 class ExchangeServerApplicationTests {
-	@Autowired
-	OrderController orderController;
 
 	@Test
 	void contextLoads() {
-		assertThat(orderController).isNotNull();
 	}
 
 }
