439abc11ca1bca0517dda7ed7d410244b27911ed
= <=>
2021-03-21 19:42:28 +0000

    publishing trade order object to pubsub

--- TradeServices/src/main/java/tradeengine/TradeServices/TradeEngineRESTController.java
+++ TradeServices/src/main/java/tradeengine/TradeServices/TradeEngineRESTController.java
@@ -1,17 +1,27 @@
 package tradeengine.TradeServices;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.GsonBuilder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+
+import java.util.List;
 
 
 @RestController
 public class TradeEngineRESTController {
 
     private Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setLenient().create())).baseUrl("https://exchange-con.herokuapp.com").build();
+    @Autowired
+    Jedis jedisclient;
+
 
 @RequestMapping(method=RequestMethod.POST, value="/api/trade", produces = "application/json", consumes= "application/json")
 @ResponseBody
@@ -23,13 +33,19 @@ public tradeengine.TradeServices.Response receiveOrder(@RequestBody Order order)
     orderwithexchange.setSide(order.getSide());
     orderwithexchange.setExchange("exchange1");    //this is where trade engine specifies exchange to trade on.
 
+    //pubsub
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+        jedisclient.publish("OrdersChannel", objectMapper.writeValueAsString(orderwithexchange));   //sending it to pub sub on channel Orders Channel
+    }catch(JsonProcessingException e){};
+
+    //reequest to rest api
     var service = retrofit.create(OrderService.class);
     var req = service.createOrder(orderwithexchange);
     try {
 //        return req.execute().body();
 
         Response<tradeengine.TradeServices.Response> res = req.execute();
-        System.out.println(res);
         tradeengine.TradeServices.Response result = res.body();
         return result;
     } catch (java.io.IOException e) {
@@ -37,4 +53,10 @@ public tradeengine.TradeServices.Response receiveOrder(@RequestBody Order order)
     }
 return new tradeengine.TradeServices.Response();
 };
+
+@RequestMapping(method = RequestMethod.POST, value="/md", consumes = "application/json")
+@ResponseBody
+public void receiveMarketData(@RequestBody List<TradeData> md){
+    System.out.println(md);
+}
 }
---
+++ TradeServices/src/main/java/tradeengine/TradeServices/jedisconfig/JedisConfig.java
@@ -0,0 +1,19 @@
+package tradeengine.TradeServices.jedisconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+
+@Configuration
+public class JedisConfig {
+
+    @Bean
+    public static Jedis createJedisClient(){
+        Jedis jedisclient = new Jedis();
+       // jedisclient.auth("fcTHon925fcjUDen1ujM4x5Ra1PsJYIk");
+        return jedisclient;
+    }
+}
+
+
+// "redis-14349.c81.us-east-1-2.ec2.cloud.redislabs.com", 14349
