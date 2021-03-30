b44808ab7e49ab3faf8e823240736e644913b28a
= <=>
2021-03-30 17:05:19 +0000

    deploying to TradeServices

--- TradeServices/src/main/java/tradeengine/TradeServices/TradeEngineRESTController.java
+++ TradeServices/src/main/java/tradeengine/TradeServices/TradeEngineRESTController.java
@@ -15,7 +15,6 @@ public class TradeEngineRESTController {
     @Autowired
     MarketData MD;
 
-    // receive data from exchange one
 @RequestMapping(method = RequestMethod.POST, value="/md", consumes = "application/json")
 @ResponseBody
 public void receiveMarketData(@RequestBody List<TradeData> md){
@@ -24,7 +23,6 @@ public void receiveMarketData(@RequestBody List<TradeData> md){
 }
 
 
-// receive market data from exhange two. handle that
 @RequestMapping(method = RequestMethod.POST, value="/md2", consumes = "application/json")
 @ResponseBody
 public void receiveMarketData2(@RequestBody List<TradeData> md){
